3400304b2c8203fba56ba60b7f47ad31e41e97d5
Guillaume Trüeb <guillaumetrueb@etik.com>
2023-11-19 09:56:16 +0100

    feat: creating the base of TCP communication

--- src/main/java/ch/heigvd/Client/Client.java
+++ src/main/java/ch/heigvd/Client/Client.java
@@ -1,4 +1,36 @@
 package ch.heigvd.Client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket("127.0.0.1", 5555);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             Scanner in = new Scanner(socket.getInputStream());
+             Scanner consoleInput = new Scanner(System.in)) {
+
+            // Lire les messages du serveur
+            System.out.println(in.nextLine());
+
+            // Boucle pour permettre à l'utilisateur d'envoyer des messages au serveur
+            while (true) {
+
+                // Ecris le message à envoyer au serveur
+                String userMessage = consoleInput.nextLine();
+
+                // Envoyer le message au serveur
+                out.println(userMessage);
+
+                // Lire la réponse du serveur
+                System.out.println(in.nextLine());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,13 +1,17 @@
 package ch.heigvd;
 
+import ch.heigvd.Client.Client;
 import ch.heigvd.PicoCLI.PicoCLI;
+import ch.heigvd.Server.Server;
 import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
 
-        CommandLine main = new CommandLine(new PicoCLI());
-        main.execute(args);
+        String serverAddress = "127.0.0.1";
+
+        Server.start(serverAddress, 5555);
+        //Client.run(serverAddress, 5555);
 
         System.exit(0);
 
